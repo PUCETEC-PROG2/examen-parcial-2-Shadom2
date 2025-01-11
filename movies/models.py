5fa003795c3d50d9639abfdfc739a3707eeb1082
Pablo Brito <pabrito@puce.edu.ec>
2025-01-11 10:50:24 -0500

    Examen parcial 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ('ACTION', 'Acción'),
+        ('COMEDY', 'Comedia'),
+        ('DRAMA', 'Drama'),
+        ('HORROR', 'Terror'),
+        ('SCI-FI', 'Ciencia Ficción'),
+        ('ROMANCE', 'Romance'),
+    ]
+
+    title = models.CharField(max_length=100)
+    genre = models.CharField(
+        max_length=50,
+        choices=GENRE_CHOICES,
+    )
+    director = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
+from .models import Movie
 from django.shortcuts import render
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all() 
+    return render(request, 'index.html', {'movies': movies})
+def add_movie(request):
+    return render(request, 'movies/add_movies')
